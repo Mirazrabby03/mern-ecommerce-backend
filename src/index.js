2f63fecf1e0abbcf95476f0931ed036bb83bbea7
unknown <mirazrabby03@gmail.com>
2021-08-08 19:31:27 +0600

    Database changed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ const express = require('express');
 const env = require('dotenv');
 const app = express();
 const path = require('path');
-require("./db/conn");
+//require("./db/conn");
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 //environmental variable 
 env.config();  
+//mongoDB connection
+//mongodb+srv://root:<password>@cluster0.5z7fm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.5z7fm.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(() =>{
+    console.log(`connection successful`);
+}).catch((e) =>{
+    console.log(`no connection`);
+})
+
 //routes
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
